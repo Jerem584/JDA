2d358a5066cf960c9ad44e156541c79d9cad2ba3
Your Name <you@example.com>
2021-12-22 03:58:15 +0100

    Better code already

--- TestProjetc/TestProject.cpp
+++ TestProjetc/TestProject.cpp
@@ -1,10 +1,12 @@
 #include <JDA.h>
 #include <iostream>
 #include <zip.h>
+#include <filesystem>
 
 int main() {
 	int error;
-	zip* file = zip_open("C:\\Users\\noctu\\source\\repos\\JDA\\jars\\NFix.jar", ZIP_RDONLY, &error);
+	auto file_path = std::filesystem::current_path() / "jars" / "NFix.jar";
+	zip* file = zip_open(file_path.string().c_str(), ZIP_RDONLY, &error);
 	JDA* jda = new JDA(file);
 	
 	
