de787247e2c802c849b4c1fa700d83ccfdb7ce38
Your Name <you@example.com>
2021-12-22 04:23:52 +0100

    Fix noctu's error

--- TestProjetc/TestProject.cpp
+++ TestProjetc/TestProject.cpp
@@ -2,14 +2,28 @@
 #include <iostream>
 #include <zip.h>
 #include <filesystem>
+#include <format>
+#include <iostream>
+#include <string>
+#include <string_view>
+
 
 int main() {
 	int error;
-	auto file_path = std::filesystem::current_path() / "jars" / "NFix.jar";
+	auto file_path = std::filesystem::current_path() / "../jars" / "NFix.jar";
 
 	zip* file = zip_open(file_path.string().c_str(), ZIP_RDONLY, &error);
 	JDA* jda = new JDA(file);
-	
+	for (auto b : jda->classBuffers)
+	{
+		auto buf = b.second;
+		buf->skip(4);
+		uint16_t minor = buf->get<uint16_t>();
+		uint16_t major = buf->get<uint16_t>();
+		uint16_t constantPoolCount = buf->get<uint16_t>();
+		std::cout << b.first << " -> " << major << std::endl;
+	}
+
 	
 	
 	return 0;
