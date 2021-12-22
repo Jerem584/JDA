21ead88d4c67cfca3f8c3688d9baf056f8b424ac
NOcturnais <NOcturnais1412@gmail.com>
2021-12-22 14:53:27 +0100

    cleaner

--- JDA/common.h
+++ JDA/common.h
@@ -9,3 +9,8 @@
 #include <unordered_map>
 #include <zip.h> // vcpkg install libzip:x64-windows && vcpkg integrate install
 #include <fstream>
+
+typedef uint8_t u1;
+typedef uint16_t u2;
+typedef uint32_t u4;
+typedef uint64_t u8;
