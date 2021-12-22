c1401361346fba7458b52eb663b60a60b514a2f8
NOcturnais <NOcturnais1412@gmail.com>
2021-12-22 04:29:03 +0100

    tkt

--- TestProjetc/TestProject.cpp
+++ TestProjetc/TestProject.cpp
@@ -11,7 +11,12 @@ int main() {
 	zip* file = zip_open(file_path.string().c_str(), ZIP_RDONLY, &error);
 	JDA* jda = new JDA(file);
 	
-	
+	ByteBuffer* buf = jda->classBuffers["fr/noctu/nfix/JoinListener.class"];
+	buf->skip(4);
+	uint16_t minor = buf->get<uint16_t>();
+	uint16_t major = buf->get<uint16_t>();
+	uint16_t constantPoolCount = buf->get<uint16_t>();
+	std::cout << constantPoolCount << std::endl;
 
 	return 0;
 }
