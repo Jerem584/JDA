3dda122fe0cd4dfc30f3f35f8b5402c12f0c3609
Your Name <you@example.com>
2021-12-22 03:58:57 +0100

    test

--- TestProjetc/TestProject.cpp
+++ TestProjetc/TestProject.cpp
@@ -6,6 +6,7 @@
 int main() {
 	int error;
 	auto file_path = std::filesystem::current_path() / "jars" / "NFix.jar";
+
 	zip* file = zip_open(file_path.string().c_str(), ZIP_RDONLY, &error);
 	JDA* jda = new JDA(file);
 	
